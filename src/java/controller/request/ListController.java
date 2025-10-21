53c61ab0f44a5577ff39b594eef55b3045d79372
ducewcgke2005 <ducvjpro04@gmail.com>
2025-10-22 01:27:45 +0700

    List Controller

--- src/java/controller/request/ListController.java
+++ src/java/controller/request/ListController.java
@@ -1,30 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller.request;
 
 import controller.iam.BaseRequiredAuthorizationController;
+import dal.LeaveRequestDBContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import model.iam.User;
+import model.LeaveRequest;
 
-/**
- *
- * @author sonnt
- */
-@WebServlet(urlPatterns = "/request/list")
+@WebServlet("/request/list")
 public class ListController extends BaseRequiredAuthorizationController {
 
     @Override
-    protected void processPost(HttpServletRequest req, HttpServletResponse resp, User user) throws ServletException, IOException {
+    protected void processGet(HttpServletRequest req, HttpServletResponse resp, User user)
+            throws ServletException, IOException {
+
+        LeaveRequestDBContext db = new LeaveRequestDBContext();
+        String view = req.getParameter("view");
+        ArrayList<LeaveRequest> list;
+
+        if (view == null || view.equals("mine")) {
+            list = db.getByEmployeeId(user.getEmployee().getId());
+        } else {
+            list = db.getBySubordinates(user.getEmployee().getId());
+        }
+
+        req.setAttribute("requests", list);
+        req.setAttribute("viewType", view);
+        req.getRequestDispatcher("/view/leave/list.jsp").forward(req, resp);
     }
 
     @Override
-    protected void processGet(HttpServletRequest req, HttpServletResponse resp, User user) throws ServletException, IOException {
-    }
-    
+    protected void processPost(HttpServletRequest req, HttpServletResponse resp, User user) {}
 }
